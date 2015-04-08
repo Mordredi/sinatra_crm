e05a248a8f5ed55f96a3ffddb98269fd00ac8551
Arthur <arthur.p.wright@gmail.com>
2015-04-08 14:50:54 -0400

    Can now add contacts and will appear on contacts page

--- crm.rb
+++ crm.rb
@@ -1,7 +1,11 @@
 require_relative 'contact'
+require_relative 'rolodex'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
 get '/' do
+  @crm_app_name = "Arthur's CRM"
   erb :index
 end
 
@@ -9,3 +13,12 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :add_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
+end
--- rolodex.rb
+++ rolodex.rb
@@ -4,6 +4,21 @@ class Rolodex
 
   def initialize
     @contacts = []
+    @id = 1000
+  end
+
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+
+  def find(id)
+    @contacts.find { |contact| contact.id == id }
+  end
+
+  def delete_contact(id)
+    @contacts.delete(find(id))
   end
 
 end
